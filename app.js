22280ce6d5e1586fbd8bf6e7917d9695c2da0d68
Andy Siradas <andys6190@gmail.com>
2016-10-08 19:32:56 -0400

    roads changing colors

---
+++ app.js
@@ -0,0 +1,53 @@
+var map;
+var mapData;
+var colors = [
+    '#FF0000',
+    '#FF3300',
+    '#ff6600',
+    '#ff9900',
+    '#FFCC00',
+    '#FFFF00',
+    '#ccff00',
+    '#99ff00',
+    '#66ff00',
+    '#33ff00'
+];
+
+function initMap() {
+    map =  new google.maps.Map(document.getElementById('map'), {
+        zoom: 14,
+        center: {lat: 43.079709181516954,  lng:  -76.139201824620429}
+    });
+
+    fetch('https://andys6190.github.io/roads/json/MergedRoadRatings2015.json').then(function(ret) {
+        return ret.json();
+    }).then(function(json) {
+        mapData = json;
+        map.data.addGeoJson(mapData);
+        setDisplayProperty('overall');
+    });
+}
+
+function setDisplayProperty(property) {
+    map.data.setStyle(function(feature) {
+        if (feature.getProperty('percent_in_poverty')) {
+            return;
+        }
+
+        var relevantProperty = feature.getProperty(property);
+
+        if (!relevantProperty) {
+            return {
+                strokeWeight: 0
+            };
+        }
+
+        relevantProperty--;
+
+        return {
+            fillColor: colors[relevantProperty],
+            strokeColor: colors[relevantProperty],
+            strokeWeight: 4
+        };
+    });
+}
