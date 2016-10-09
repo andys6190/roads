095f3c8478d20ebdd5b80adbca80e3099b7b8de7
Andy Siradas <andys6190@gmail.com>
2016-10-08 20:07:02 -0400

    self envoking function

--- app.js
+++ app.js
@@ -1,53 +1,64 @@
-var map;
-var mapData;
-var colors = [
-    '#FF0000',
-    '#FF3300',
-    '#ff6600',
-    '#ff9900',
-    '#FFCC00',
-    '#FFFF00',
-    '#ccff00',
-    '#99ff00',
-    '#66ff00',
-    '#33ff00'
-];
+var app = (function() {
+    var map = null;
+    var mapData = null;
+    var colors = [
+        '#FF0000',
+        '#FF3300',
+        '#ff6600',
+        '#ff9900',
+        '#FFCC00',
+        '#FFFF00',
+        '#ccff00',
+        '#99ff00',
+        '#66ff00',
+        '#33ff00'
+    ];
+    var filters = {
+        year: 2015,
+        property: 'overall'
+    };
 
-function initMap() {
-    map =  new google.maps.Map(document.getElementById('map'), {
-        zoom: 14,
-        center: {lat: 43.079709181516954,  lng:  -76.139201824620429}
-    });
+    function init() {
+        map =  new google.maps.Map(document.getElementById('map'), {
+            zoom: 14,
+            center: {lat: 43.079709181516954,  lng:  -76.139201824620429}
+        });
 
-    fetch('https://andys6190.github.io/roads/json/MergedRoadRatings2015.json').then(function(ret) {
-        return ret.json();
-    }).then(function(json) {
-        mapData = json;
-        map.data.addGeoJson(mapData);
-        setDisplayProperty('overall');
-    });
-}
+        fetch('https://andys6190.github.io/roads/json/MergedRoadRatings2015.json').then(function(ret) {
+            return ret.json();
+        }).then(function(json) {
+            mapData = json;
+            map.data.addGeoJson(mapData);
+            setDisplayProperty('overall');
+        });
+    }
 
-function setDisplayProperty(property) {
-    map.data.setStyle(function(feature) {
-        if (feature.getProperty('percent_in_poverty')) {
-            return;
-        }
+    function setDisplayProperty(property) {
+        map.data.setStyle(function(feature) {
+            if (feature.getProperty('percent_in_poverty')) {
+                return;
+            }
 
-        var relevantProperty = feature.getProperty(property);
+            var relevantProperty = feature.getProperty(property);
 
-        if (!relevantProperty) {
+            if (!relevantProperty) {
+                return {
+                    strokeWeight: 0
+                };
+            }
+
+            relevantProperty--;
+
             return {
-                strokeWeight: 0
+                fillColor: colors[relevantProperty],
+                strokeColor: colors[relevantProperty],
+                strokeWeight: 4
             };
-        }
+        });
+    }
 
-        relevantProperty--;
-
-        return {
-            fillColor: colors[relevantProperty],
-            strokeColor: colors[relevantProperty],
-            strokeWeight: 4
-        };
-    });
-}
+    return {
+        init: init,
+        setDisplayProperty: setDisplayProperty
+    };
+})();
